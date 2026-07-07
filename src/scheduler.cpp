7e00905f83b661a9a6ca5a03b8523f00379bccad
abhinav Dubey <abhinavdube@hotmail.com>
2026-07-07 09:21:54 +0900

    Added implementation for scheduler class methods  - RegisterService  - ReportFailure  - GetState

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -3,17 +3,29 @@
 bool Scheduler::RegisterService(
     const std::string& serviceName,
     std::vector<std::shared_ptr<IRecoveryAction>> actionSequence) {
- // to implement
+  if (actionSequence.empty()) {
     return false;
+  }
 
+  const auto [it, inserted] = services_.emplace(
+      serviceName, ServiceRecoveryState(std::move(actionSequence)));
+  return inserted;
 }
 
 std::optional<std::string> Scheduler::ReportFailure(const std::string& serviceName) {
-  // to implement
- return std::nullopt;
+  const auto it = services_.find(serviceName);
+  if (it == services_.end()) {
+    return std::nullopt;
+  }
+
+  return it->second.HandleFailure(serviceName).GetName();
 }
 
 std::optional<ServiceStatus> Scheduler::GetState(const std::string& serviceName) const {
-    // to implement
- return std::nullopt;
+  const auto it = services_.find(serviceName);
+  if (it == services_.end()) {
+    return std::nullopt;
+  }
+
+  return ServiceStatus{it->second.GetLevel(), it->second.GetLastAction()};
 }
